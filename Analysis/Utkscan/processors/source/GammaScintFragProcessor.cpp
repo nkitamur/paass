b47279eccc82bfc9d4acb506080e35cae13e45ae
nkitamur <75448391+nkitamur@users.noreply.github.com>
2022-05-05 00:50:11 -0400

    Revert "In GammaScintFragProcessor, switched time from timestamp to on-board cfd time."
    This reverts commit 012dab9c59bc7cc9ccc4b3cb2e7623db18a132e7.

--- Analysis/Utkscan/processors/source/GammaScintFragProcessor.cpp
+++ Analysis/Utkscan/processors/source/GammaScintFragProcessor.cpp
@@ -187,10 +187,7 @@ bool GammaScintFragProcessor::Process(RawEvent &event) {
         string subType = (*it)->GetChanID().GetSubtype();
         unsigned int subTypeOffset = ReturnOffset(subType);
         double Genergy = (*it)->GetCalibratedEnergy();
-        //double Gtime = (*it)->GetTimeSansCfd() * Globals::get()->GetClockInSeconds(currentModFreq) * 1.e9;
-        //modify by Xu to use onboard cfd timing
-        double internalTAC_Convert_Tick_adc = Globals::get()->GetAdcClockInSeconds((*it)->GetChanID().GetModFreq()) * 1e9;
-        double Gtime = (*it)->GetTime() * internalTAC_Convert_Tick_adc;
+        double Gtime = (*it)->GetTimeSansCfd() * Globals::get()->GetClockInSeconds(currentModFreq) * 1.e9;
 
         if (!((*it)->GetChanID().HasTag("dy"))) {
             plot(D_ENERGY + subTypeOffset, Genergy);
